98ee862fa961f9adc46269c49a719899446a7875
jayabal velusamy <Jayabal.Velusamy@egonzehnder.com>
2022-12-03 11:33:11 +0530

    Adding juice shop application testcase

--- src/test/java/tests/RegisterUserTest_FirstTest.java
+++ src/test/java/tests/RegisterUserTest_FirstTest.java
@@ -1,8 +1,10 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -10,36 +12,52 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class SearchAndAddToCartTest_FirstTest {
+public class RegisterUserTest_FirstTest {
 
     @Test
-    public void searchAndAddToCartTest(){
-        WebDriver driver= new ChromeDriver();
-        driver.get("http://spree-vapasi.herokuapp.com/");
+    public void searchAndAddToCartTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://juice-shop.herokuapp.com/#/");
 
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
-        driver.findElement(By.id("keywords")).sendKeys("Jersey");
 
-        driver.findElement(By.xpath("//input[@value='Search']")).click();
+        driver.findElement(By.xpath("//button[@color='primary']")).click();
+        driver.findElement(By.linkText("Me want it!")).click();
 
-        assertTrue(driver.findElement(By.xpath("//span[contains(text(),'Jersey')]"))
+        driver.findElement(By.id("navbarAccount")).click();
+        driver.findElement(By.id("navbarLoginButton")).click();
+
+        assertEquals(driver.findElement(By.xpath("//h1")).getText(),"Login");
+        driver.findElement(By.id("email")).sendKeys("jaya@sample.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("jaya@sample.com");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        assertTrue(driver.findElement(By.xpath("//span[text()=' Your Basket']"))
                 .isDisplayed());
+        driver.findElement(By.xpath("//mat-icon[text()=' search ']")).click();
+        driver.findElement(By.xpath("//input[contains(@id,'mat-input')]")).sendKeys("apple");
 
-        String selectedProduct= driver.findElement(By.xpath("(//span[contains(text(),'Jersey')])[1]")).getText();
+        Actions actions=new Actions(driver);
+        actions.sendKeys(Keys.ENTER).build().perform();
 
-        driver.findElement(By.xpath("(//span[contains(text(),'Jersey')])[1]")).click();
+        assertTrue(driver.findElement(By.xpath("//div[contains(text(),'Apple')]"))
+                .isDisplayed());
+        String selectedProduct=driver.findElement(By.xpath("(//div[contains(text(),'Apple')])[1]")).getText();
 
+        driver.findElement(By.xpath("(//div[contains(text(),'Apple')])[1]")).click();
 
-        assertEquals(driver.findElement(By.cssSelector(".product-title")).getText(),
-                selectedProduct);
-        driver.findElement(By.cssSelector("#add-to-cart-button")).click();
+        driver.findElement(By.xpath("//span[text()='Reviews']")).click();
+        driver.findElement(By.xpath("//span[text()=' Close']")).click();
 
-        assertEquals(driver.findElement(By.xpath("//div[@data-hook='cart_container']/h1")).getText(), "Shopping Cart");
-        assertEquals(driver.findElement(By.xpath("//h4/a")).getText(),
-                selectedProduct);
-        assertEquals(driver.findElement(By.xpath("//button[@name='checkout']")).getText(), "Checkout");
+        driver.findElement(By.xpath("//span[text()='Add to Basket']")).click();
+
+        driver.findElement(By.xpath("//span[text()=' Your Basket']")).click();
+
+        assertEquals(driver.findElement(By.xpath("//h1")).getText(),"Your Basket");
+
+        assertEquals(driver.findElement(By.xpath("//mat-cell[@class='mat-cell cdk-cell cdk-column-product mat-column-product ng-star-inserted']")).getText(),selectedProduct);
 
         driver.close();
         driver.quit();
